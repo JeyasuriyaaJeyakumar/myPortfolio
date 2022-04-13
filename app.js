10fcec54fe15e10603357b9c1d4b1e20356f1f51
JeyasuriyaaJeyakumar <103479902+JeyasuriyaaJeyakumar@users.noreply.github.com>
2022-04-13 22:06:55 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexController = require('./controllers/index');
-var projectsController = require('./controllers/projects');
-
+var usersController = require('./controllers/users');
 
 var app = express();
 
@@ -21,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexController);
-app.use('/projects', projectsController);
+app.use('/users', usersController);
 
 
 // catch 404 and forward to error handler
